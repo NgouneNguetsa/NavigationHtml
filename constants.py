7bdf5b60f0a66b0293a95706774c270e17c7e8da
Garlan Ngoune Nguetsa <garlan.ngoune-nguetsa@etu.umontpellier.fr>
2026-05-17 21:19:07 -0400

    dev 41 - Finally solve the problem with ThreadInterrupt

--- constants.py
+++ constants.py
@@ -37,7 +37,7 @@ class Constante:
     def DisableGlobalListener():
         Constante.globalListener_disabled.set()
         
-    def ThreadInterrupt(self):
+    def ThreadInterrupt(self, *args, **kwargs):
         time.sleep(5)
         try:
             keyboard.unblock_key("left")
