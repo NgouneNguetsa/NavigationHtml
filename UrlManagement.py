4aacb21ada0d1e07521e8908bc886f47eca7bd49
Garlan Ngoune Nguetsa <garlan.ngoune-nguetsa@etu.umontpellier.fr>
2026-08-16 19:41:59 -0400

    V4.1.2 - Change the click from a space in the screen to the scroll bar

--- UrlManagement.py
+++ UrlManagement.py
@@ -116,11 +116,11 @@ class Url:
     def mouseMove(x, y):
         pyautogui.click(x, y)
 
-        pyautogui.moveTo(Constante.halfScreenWidth, Constante.halfScreenHeight)            
+        pyautogui.moveTo(Constante.screenWidth, y)
         time.sleep(2)
 
         pyautogui.leftClick()
-        pyautogui.moveTo(Constante.screenWidth, y)
+        pyautogui.hotkey('shift' ,'space')
 
     @testScreenCorners
     def mouseMoveAlternative(x, y):
